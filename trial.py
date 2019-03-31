81ed62b552f201c693613ba5adde403527e96a26
celina-iris <celina_brito@dlsu.edu.ph>
2019-04-01 00:20:30 +0800

    Change scores and command keywords

--- trial.py
+++ trial.py
@@ -1,21 +1,73 @@
 from src.dialoguemanager import Follow_Up, DBO_Follow_Up
+from src.db.concepts import DBO_Concept, DBO_Local_Concept
+from numpy import random
 
-print("hello")
+def get_string_template(template2):
+    final_response = ""
+    for j in range(len(template2)):
+        final_response += template2[j]
+    return final_response
 
-k = DBO_Follow_Up.get_specific_follow_up_template(2)
-# k.print() #Check if na lilipat
+template = "_character_ used _A_ to _B_" #for replacing
+assertionsList= []
+assertionsList.append("character CapableOf B")
+assertionsList.append("A UsedFor B")
+print(template)
+
+searchConcept = []
+searchConcept.append("first")
+searchConcept.append("second")
+
+replace = ["character", "A", "B"]
+position = [1, 3, 5]
+
+template2 = template.split("_")
+#print(template2)
 
 #DICTIONARY
 dict = {}
-dict["character"] = "Irene"
-dict["start"] = "mic"
-dict["end"] = "sing"
+dict[template2[1]] = "Irene"
+dict[template2[3]] = "mic"
+dict[template2[5]] = "sing"
 
-k.blank_dictionary = dict
-##DICTIONARY
+#print(dict[template2[1]])
+tempChar = ""
 
-k.split_template()
+template2[1] = "Mary"
+conceptList= []
 
-k.fill_blank_template()
+for i in range(len(assertionsList)):
+    tempList = assertionsList[i].split(" ")
+    first1 = tempList[0]
+    relation1 = tempList[1]
+    second1 = tempList[2]
 
-k.get_string_template()
+    if tempChar != "":
+        second1 = tempChar
+
+    if searchConcept[i] == "first":
+        usable_concepts = DBO_Concept.get_concept_like(relation1, first=first1)
+
+    else:
+        usable_concepts = DBO_Concept.get_concept(second1, relation1)
+    
+        while len(usable_concepts) == 0:
+            usable_concepts = DBO_Concept.get_concept_like(relation1, first=first1)
+            concept_index = random.randint(0,len(usable_concepts))
+            concept = usable_concepts[concept_index]
+            conceptList[0] = concept
+    
+    if len(usable_concepts) != 0:
+        concept_index = random.randint(0,len(usable_concepts))
+        concept = usable_concepts[concept_index]
+        tempChar = concept.second
+        conceptList.append(concept)
+    
+    if i == len(assertionsList)-1:
+        template2[3] = concept.first
+        template2[5] = concept.second
+
+print("Assertion 0: ", conceptList[0])
+print("Assertion 1: ", conceptList[1])
+p = get_string_template(template2)
+print(p)
